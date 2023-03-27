953a019103c5ac99a04c57e852806278e33afa62
Oignnon - <td665pro@pm.me>
2023-03-27 07:22:20 +0200

    First commit

---
+++ index.js
@@ -0,0 +1,3 @@
+ReactDOM.render(<h1>Hello everyone!</h1>, document.getElementById("root"))
+
+// Watch out, using React 17 if writing render. creatRoot = newer React 18!
---
+++ readme.md
@@ -0,0 +1,1 @@
+Imported these links to use react. In other course did it in typescript files! https://legacy.reactjs.org/docs/cdn-links.html
